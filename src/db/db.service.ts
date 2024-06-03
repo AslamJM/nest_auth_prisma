6522056d80cb00b63775adc0ca2b82df04feb09c
AslamJM-SPMPC <jma8744@gmail.com>
2024-06-03 10:33:58 +0530

    created db module init prisma

---
+++ src/db/db.service.ts
@@ -0,0 +1,10 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class DbService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+
+    async onModuleInit() {
+        await this.$connect()
+    }
+}
